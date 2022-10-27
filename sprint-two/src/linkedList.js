3dba2f67044b0a16eee06ae412bf421cf79b4542
sindwarf <sindorfsolutions@gmail.com>
2022-10-26 18:18:36 -0700

    significant progress on linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //i: any numeric value
+  //create new node with new value
+      //node.next will stay null
+  //if head doesn't exist
+      //point head to new node
+  //if tail exists
+      //modify the existing tail
+          //node.next of tail will point to new node
+
+    var newNode = Node(value);
+    if (list.head === null) { //if linked list is empty
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    //o: return removed node value
+    //if head exists
+    //if next node does not exist
+        //store first node
+        //set head and tail to null
+        //
+    //else
+    //next node becomes head
+    //store first node
+    //set thisnode.nextnode
+    //return node
+    var returnNode = list.head;
+    if (!list.head.next) {
+      list.tail = null;
+      list.head = null;
+    } else {
+      list.head = returnNode.next;
+    }
+    return returnNode.value;
   };
 
   list.contains = function(target) {
+    //i: target node
+    //o: boolean
   };
 
   return list;
